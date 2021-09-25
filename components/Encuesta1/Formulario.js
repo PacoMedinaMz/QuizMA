40e69b731189f482a24603146de47045251fcbd7
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-09-25 16:23:27 -0500

    Pregunta 5

--- components/Encuesta1/Formulario.js
+++ components/Encuesta1/Formulario.js
@@ -4,12 +4,14 @@ import Pregunta1 from "./preguntas/Pregunta1";
 import Pregunta2 from "./preguntas/Pregunta2";
 import Pregunta3 from "./preguntas/Pregunta3";
 import Pregunta4 from "./preguntas/Pregunta4";
+import Pregunta5 from "./preguntas/Pregunta5";
 
 const Formulario = () => {
   const [genero, setGenero] = useState("na");
   const [edad, setEdad] = useState("1");
   const [orientacion, setOrientacion] = useState("0");
   const [metodos, setMetodos] = useState([]);
+  const [conocimientos, setConocimientos] = useState("0");
 
   return (
     <View
@@ -21,6 +23,7 @@ const Formulario = () => {
       <Pregunta2 value={edad} setValue={setEdad} />
       <Pregunta3 value={orientacion} setValue={setOrientacion} />
       <Pregunta4 value={metodos} setValue={setMetodos} />
+      <Pregunta5 value={conocimientos} setValue={setConocimientos} />
     </View>
   );
 };
