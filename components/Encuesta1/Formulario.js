90d1a7f1bf9d5e968208f6961b5597e73beaed2e
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-09-25 15:53:05 -0500

    Pregunta 3

--- components/Encuesta1/Formulario.js
+++ components/Encuesta1/Formulario.js
@@ -2,10 +2,12 @@ import React, { Fragment, useState } from "react";
 import { View } from "react-native";
 import Pregunta1 from "./preguntas/Pregunta1";
 import Pregunta2 from "./preguntas/Pregunta2";
+import Pregunta3 from "./preguntas/Pregunta3";
 
 const Formulario = () => {
   const [genero, setGenero] = useState("na");
   const [edad, setEdad] = useState("1");
+  const [orientacion, setOrientacion] = useState("0");
 
   return (
     <View
@@ -15,6 +17,7 @@ const Formulario = () => {
     >
       <Pregunta1 value={genero} setValue={setGenero} />
       <Pregunta2 value={edad} setValue={setEdad} />
+      <Pregunta3 value={orientacion} setValue={setOrientacion} />
     </View>
   );
 };
---
+++ components/Encuesta1/preguntas/Pregunta3.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Pregunta from "../Pregunta";
+import { Picker } from "@react-native-picker/picker";
+
+const Pregunta3 = ({ value, setValue }) => {
+  return (
+    <Pregunta title="¿En qué grado has recibido educación sexual?">
+      <Picker
+        selectedValue={value}
+        onValueChange={(itemValue) => setValue(itemValue)}
+        style={{
+          width: "100%",
+          height: 40,
+        }}
+      >
+        <Picker.Item label="Nula" value="0" />
+        <Picker.Item label="Mala" value="1" />
+        <Picker.Item label="Mínima" value="2" />
+        <Picker.Item label="Regular" value="3" />
+        <Picker.Item label="Básica" value="4" />
+        <Picker.Item label="Buena" value="5" />
+        <Picker.Item label="Excelente" value="6" />
+      </Picker>
+    </Pregunta>
+  );
+};
+
+export default Pregunta3;
