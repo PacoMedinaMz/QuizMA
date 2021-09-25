38c19ec8cd9f18ebfc3560db0d0b68587bdc9b82
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-09-25 16:33:57 -0500

    Botones de acción

--- components/Encuesta1/Formulario.js
+++ components/Encuesta1/Formulario.js
@@ -1,5 +1,7 @@
-import React, { Fragment, useState } from "react";
-import { View } from "react-native";
+import { useNavigation } from "@react-navigation/core";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Card } from "react-native-paper";
 import Pregunta1 from "./preguntas/Pregunta1";
 import Pregunta2 from "./preguntas/Pregunta2";
 import Pregunta3 from "./preguntas/Pregunta3";
@@ -7,11 +9,24 @@ import Pregunta4 from "./preguntas/Pregunta4";
 import Pregunta5 from "./preguntas/Pregunta5";
 
 const Formulario = () => {
+  const navigation = useNavigation();
+
   const [genero, setGenero] = useState("na");
   const [edad, setEdad] = useState("1");
   const [orientacion, setOrientacion] = useState("0");
   const [metodos, setMetodos] = useState([]);
   const [conocimientos, setConocimientos] = useState("0");
+
+  const enviar = () => {
+    const data = {
+      genero: genero,
+      edad: edad,
+      orientacion: orientacion,
+      metodos: metodos,
+      conocimientos: conocimientos,
+    };
+    console.log(data);
+  };
 
   return (
     <View
@@ -24,8 +39,44 @@ const Formulario = () => {
       <Pregunta3 value={orientacion} setValue={setOrientacion} />
       <Pregunta4 value={metodos} setValue={setMetodos} />
       <Pregunta5 value={conocimientos} setValue={setConocimientos} />
+
+      <Card.Actions style={styles.cardbtn}>
+        {/* Regresar btn */}
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.boton}
+        >
+          <Text style={styles.botonOk}>Regresar</Text>
+        </TouchableOpacity>
+
+        {/* Continuar btn */}
+        <TouchableOpacity onPress={() => enviar()} style={styles.boton}>
+          <Text style={styles.botonOk}>Enviar</Text>
+        </TouchableOpacity>
+      </Card.Actions>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  cardbtn: {
+    width: "100%",
+  },
+  boton: {
+    width: "50%",
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  botonOk: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "#fff",
+    backgroundColor: "#FF4646",
+    borderRadius: 15,
+    padding: 4,
+    margin: 5,
+    marginLeft: 10,
+  },
+});
 
 export default Formulario;
